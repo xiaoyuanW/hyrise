29c88cef288abe2b3afdcbe778ab236a17834ec1
Fabian Wiebe <fabian.wiebe@student.hpi.de>
2018-08-22 17:22:32 +0200

    added bash evaluation scripts

---
+++ compare.sh
@@ -0,0 +1,51 @@
+if [ "$#" -lt 1 ]; then
+    echo "Provide dir for results"
+else
+	dir=$1
+	if [ ! -d $dir ]; then
+  		echo "Directory $dir does not exist"
+		exit
+	fi
+	if [ "${dir: -1}" != "/" ]; then
+		dir=$dir"/"
+	fi
+	if [ "$#" -eq 1 ]; then
+		cat ${dir}*.txt
+		echo no jit vs jit without mvcc
+	    python scripts/compare_benchmarks.py ${dir}result_wo_jit_wo_mvcc.json ${dir}result_w_jit_wo_mvcc.json
+		echo no jit vs jit with mvcc
+	    python scripts/compare_benchmarks.py ${dir}result_wo_jit_w_mvcc.json ${dir}result_w_ll_w_jv.json
+		echo no jit vs jit with mvcc but not jit validate
+	    python scripts/compare_benchmarks.py ${dir}result_wo_jit_w_mvcc.json ${dir}result_w_ll_wo_jv.json
+		echo lazy load
+		python scripts/compare_benchmarks.py ${dir}result_wo_ll_wo_jv.json ${dir}result_w_ll_wo_jv.json
+		echo jit validate
+		python scripts/compare_benchmarks.py ${dir}result_wo_ll_wo_jv.json ${dir}result_wo_ll_w_jv.json
+		echo lazy load with jit validate enabled
+		python scripts/compare_benchmarks.py ${dir}result_wo_ll_w_jv.json ${dir}result_w_ll_w_jv.json
+		echo jit validate with lazy load enabled
+		python scripts/compare_benchmarks.py ${dir}result_w_ll_wo_jv.json ${dir}result_w_ll_w_jv.json
+		echo lazy load and jit validate
+		python scripts/compare_benchmarks.py ${dir}result_wo_ll_wo_jv.json ${dir}result_w_ll_w_jv.json
+	else
+		r_dir=$2
+		if [ ! -d $r_dir ]; then
+	  		echo "Right directory $dir does not exist"
+			exit
+		fi
+		if [ "${r_dir: -1}" != "/" ]; then
+			r_dir=$r_dir"/"
+		fi
+		echo "prev:"
+		cat ${dir}*.txt
+		echo "next:"
+		cat ${r_dir}*.txt
+		declare -a files=("result_wo_jit_w_mvcc.json" "result_wo_jit_wo_mvcc.json" "result_wo_ll_wo_jv.json" "result_w_ll_wo_jv.json" "result_wo_ll_w_jv.json" "result_w_ll_w_jv.json" "result_w_jit_wo_mvcc.json")
+		for i in "${files[@]}"
+		do
+			echo $i
+			python scripts/compare_benchmarks.py ${dir}${i} ${r_dir}${i}
+		done
+	fi
+fi
+
---
+++ scripts/evaluate.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import json
+import sys
+from terminaltables import AsciiTable
+from termcolor import colored
+from scipy.stats import ttest_ind
+from array import array
+
+def format_diff(diff, swap = False):
+	if diff >= 0:
+		return colored('+' + "{0:.0%}".format(diff), 'green' if swap == False else 'red')
+	else:
+		return colored("{0:.0%}".format(diff), 'red' if swap == False else 'green')
+
+def calc_sum(operators):
+	preparation_time = 0
+	execution_time = 0
+	for operator in operators:
+		preparation_time += int(operator['preparation_time'])
+		execution_time += int(operator['execution_time'])
+	return (preparation_time, execution_time, preparation_time + execution_time)
+
+if(len(sys.argv) != 3):
+	print("Usage: " + sys.argv[0] + " benchmark1.json benchmark2.json")
+	exit()
+
+with open(sys.argv[1]) as old_file:
+    old_data = json.load(old_file)
+
+with open(sys.argv[2]) as new_file:
+    new_data = json.load(new_file)
+
+def print_operators(query, context, description):
+	print(description + ' query: ' + query['name'] + ', Jit: ' + context['jit'] + ', lazy load: ' + context['lazy_load'] + ', Jit validate: ' + context['jit_validate'])
+	table_data = []
+	table_data.append(['Operator          ', 'Prepare', 'Execution', 'Total time', 'share Prepare (%)', 'share Execution (%)', 'share Total (%)'])
+	prepare, execute, total = calc_sum(query['operators'])
+	for row in query['operators']:
+		row_prepare = int(row['preparation_time'])
+		row_execute = int(row['execution_time'])
+		row_total = row_prepare + row_execute
+		table_data.append([row['name'], row_prepare, row_execute, row_total,
+			"%.1f" % ((float(row_prepare)/prepare * 100) if prepare != 0 else 0.), "%.1f" % (float(row_execute)/execute* 100), "%.1f" % (float(row_total)/total*100) ])
+	table_data.append(['Total', prepare, execute, total, '', '', ''])
+	table = AsciiTable(table_data)
+	for i in range(1, 7):
+		table.justify_columns[i] = 'right'
+
+	print(table.table)
+	print("")
+
+for old, new in zip(old_data['benchmarks'], new_data['benchmarks']):
+	print_operators(old, old_data['context'], 'Old')
+	print_operators(new, new_data['context'], 'New')
+
+	print('Query: ' + old['name'])
+	table_data = []
+	table_data.append(['Time (micro s)', 'Old', 'New', 'Share (%)', 'Diff %'])
+	old_prepare, old_execute, old_total = calc_sum(old['operators'])
+	new_prepare, new_execute, new_total = calc_sum(new['operators'])
+	rows = [
+		("items / s", float(old['items_per_second']), float(new['items_per_second']), True),
+		("avg iteration", int(old['avg_real_time_per_iteration']), int(new['avg_real_time_per_iteration']), False),
+		("prepare operators", old_prepare, new_prepare, False),
+		("execute operators", old_execute, new_execute, False),
+		("total operators", old_total, new_total, False)
+	]
+	table = AsciiTable(table_data)
+	for row_value in rows:
+		description, old, new, low_is_good = row_value
+		share = ("%.2f" % (float(new) / old * 100)) if old != 0 else ''
+		diff = format_diff(float(new) / old - 1, low_is_good) if old != 0 else ''
+		if low_is_good:
+			old = "%.6f" % old
+			new = "%.6f" % new
+		table_data.append([description, old, new, share, diff])
+
+	for i in range(1, 6):
+		table.justify_columns[i] = 'right'
+
+	print(table.table)
+	print("")
