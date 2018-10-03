c4499d531efbf118fef37b5d4a7a1a6c96483962
Fabian Wiebe <fabian.wiebe@student.hpi.de>
2018-10-03 03:16:04 +0200

    Update template instantiation

--- src/lib/operators/jit_operator/operators/jit_segment_reader.cpp
+++ src/lib/operators/jit_operator/operators/jit_segment_reader.cpp
@@ -1,30 +1,50 @@
 #include "jit_segment_reader.hpp"
 
 #include <boost/preprocessor/seq/for_each.hpp>
-#include <storage/value_segment/value_segment_iterable.hpp>
+#include "storage/value_segment/value_segment_iterable.hpp"
+#include "storage/reference_segment/reference_segment_iterable.hpp"
+#include "storage/dictionary_segment/dictionary_segment_iterable.hpp"
 
 namespace opossum {
 
-#define JIT_EXPLICIT_INSTANTIATION(r, type) \
+#define U_INTS (uint8_t)(uint16_t)(uint32_t)
+
+#define JIT_EXPLICIT_INSTANTIATION_W_TYPE(r, _, type) \
   template class JitSegmentReaderWrapper<JitSegmentReader<AnySegmentIterator<type>, type, true>>; \
-  template class JitSegmentReaderWrapper<JitSegmentReader<AnySegmentIterator<type>, type, false>>; //\
-  //template class JitSegmentReaderWrapper<JitSegmentReader<ValueSegmentIterable<type>::NonNullIterator::ValueIterator, type, false>>; //\
-  //template class JitSegmentReaderWrapper<JitSegmentReader<ValueSegmentIterable<type>::Iterator::ValueIterator, type, true>>;
+  template class JitSegmentReaderWrapper<JitSegmentReader<AnySegmentIterator<type>, type, false>>; \
+  template class JitSegmentReaderWrapper<JitSegmentReader<ValueSegmentIterable<type>::Iterator, type, true>>; \
+  template class JitSegmentReaderWrapper<JitSegmentReader<ValueSegmentIterable<type>::NonNullIterator, type, false>>; \
+  template class JitSegmentReaderWrapper<JitSegmentReader<ReferenceSegmentIterable<type>::Iterator, type, true>>; \
+  template class JitSegmentReaderWrapper<JitSegmentReader<ReferenceSegmentIterable<type>::Iterator, type, false>>;
 
-JIT_EXPLICIT_INSTANTIATION(_, int)
-JIT_EXPLICIT_INSTANTIATION(_, long)
-JIT_EXPLICIT_INSTANTIATION(_, float)
-JIT_EXPLICIT_INSTANTIATION(_, double)
+#define JIT_EXPLICIT_INSTANTIATION(r, _, type) JIT_EXPLICIT_INSTANTIATION_W_TYPE(r, _, BOOST_PP_TUPLE_ELEM(3, 0, type))
 
-// template class JitSegmentReaderWrapper<JitSegmentReader<SegmentIterator<type>, type, false>>;
-template class JitSegmentReader<ValueSegmentIterable<int>::NonNullIterator, int, false>;
-template class JitSegmentReaderWrapper<JitSegmentReader<ValueSegmentIterable<int>::NonNullIterator, int, false>>;
+// template class JitSegmentReader<ValueSegmentIterable<type>::Iterator, type, true>; \
+  // template class JitSegmentReader<ValueSegmentIterable<type>::NonNullIterator, type, false>; \
+
+// JIT_EXPLICIT_INSTANTIATION(_, int)
+// JIT_EXPLICIT_INSTANTIATION(_, long)
+// JIT_EXPLICIT_INSTANTIATION(_, float)
+// JIT_EXPLICIT_INSTANTIATION(_, double)
+// template class DictionarySegmentIterable<int32_t, pmr_vector<uint8_t>>;
+// template class DictionarySegmentIterable<int, pmr_vector<int>>;
+// template class DictionarySegmentIterable<std::string, opossum::FixedStringVector>;
+
+// template class DictionarySegmentIterable<int, pmr_vector<int>>;
+
+// template class DictionarySegmentIterable<int, pmr_vector<int>>::Iterator<FixedSizeByteAlignedVector<uint8_t>>;
+
+// template class JitSegmentReaderWrapper<JitSegmentReader<DictionarySegmentIterable<int, pmr_vector<int>>::Iterator<FixedSizeByteAlignedVector<uint8_t>>, int, false>>;
+
+
+#define TYPES_WO_STRING (int32_t)(int64_t)(float)(double)(std::string)
+BOOST_PP_SEQ_FOR_EACH(JIT_EXPLICIT_INSTANTIATION, _, DATA_TYPE_INFO)
+
 // JIT_EXPLICIT_INSTANTIATION(_, std::string)
 
 // BOOST_PP_SEQ_FOR_EACH(JIT_EXPLICIT_INSTANTIATION, DATA_TYPES)
-// BOOST_PP_SEQ_FOR_EACH(JIT_EXPLICIT_INSTANTIATION, DATA_TYPES)
 
 #undef JIT_EXPLICIT_INSTANTIATION
-// template class JitValidate<TableType::References>;
+#undef JIT_EXPLICIT_INSTANTIATION_W_TYPE
 
 }  // namespace opossum
--- src/lib/operators/jit_operator/specialization/jit_code_specializer.cpp
+++ src/lib/operators/jit_operator/specialization/jit_code_specializer.cpp
@@ -194,7 +194,7 @@ void JitCodeSpecializer::_inline_function_calls(SpecializationContext& context) 
     auto function_has_opossum_namespace =
         boost::starts_with(function_name, "_ZNK7opossum") || boost::starts_with(function_name, "_ZN7opossum");
 
-    print = true;  // boost::contains(function_name, "read_value");
+    print = boost::contains(function_name, "read_value");
 
     // A note about "__clang_call_terminate":
     // __clang_call_terminate is generated / used internally by clang to call the std::terminate function when exception
